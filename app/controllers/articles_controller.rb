4163c165e8a9f2998e5fb4943127b45a78113569
Carmen Kerim <carmen.kerim01@stclairconnect.ca>
2019-04-09 20:41:01 -0400

    Preparing for application launch

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,10 +8,12 @@
 #  category   :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
 #
 
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /articles
   # GET /articles.json
@@ -40,11 +42,11 @@ class ArticlesController < ApplicationController
 
     respond_to do |format|
       if @article.save
-        format.html { redirect_to @article, notice: 'Article was successfully created.' }
-        format.json { render :show, status: :created, location: @article }
+        format.html {redirect_to @article, notice: 'Article was successfully created.'}
+        format.json {render :show, status: :created, location: @article}
       else
-        format.html { render :new }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
+        format.html {render :new}
+        format.json {render json: @article.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -54,11 +56,11 @@ class ArticlesController < ApplicationController
   def update
     respond_to do |format|
       if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-        format.json { render :show, status: :ok, location: @article }
+        format.html {redirect_to @article, notice: 'Article was successfully updated.'}
+        format.json {render :show, status: :ok, location: @article}
       else
-        format.html { render :edit }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
+        format.html {render :edit}
+        format.json {render json: @article.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -68,20 +70,30 @@ class ArticlesController < ApplicationController
   def destroy
     @article.destroy
     respond_to do |format|
-      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html {redirect_to articles_url, notice: 'Article was successfully destroyed.'}
+      format.json {head :no_content}
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def article_params
-      params.require(:article).permit(:title, :content, :category)
-      # Students, make sure to add the user_id parameter as a symbol here ^^^^^^
+  # Use callbacks to share common setup or constraints between actions.
+  def set_article
+    @article = Article.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The article you're looking for cannot be found"
+    respond_to do |format|
+      format.html {
+        redirect_to articles_path
+      }
+
+      format.json {render :json, status: 404}
     end
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def article_params
+    params.require(:article).permit(:title, :content, :category, :user_id)
+    # Students, make sure to add the user_id parameter as a symbol here ^^^^^^
+  end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,10 +8,12 @@
 #  article_id :integer
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
 #
 
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /comments
   # GET /comments.json
@@ -40,11 +42,11 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
+        format.html {redirect_to @comment, notice: 'Comment was successfully created.'}
+        format.json {render :show, status: :created, location: @comment}
       else
-        format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
+        format.html {render :new}
+        format.json {render json: @comment.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -54,11 +56,11 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
+        format.html {redirect_to @comment, notice: 'Comment was successfully updated.'}
+        format.json {render :show, status: :ok, location: @comment}
       else
-        format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
+        format.html {render :edit}
+        format.json {render json: @comment.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -68,20 +70,28 @@ class CommentsController < ApplicationController
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html {redirect_to comments_url, notice: 'Comment was successfully destroyed.'}
+      format.json {head :no_content}
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def comment_params
-      params.require(:comment).permit(:message, :visible, :article_id)
-      # Students, make sure to add the user_id and article ID parameter as symbols here ^^^^^^
+  # Use callbacks to share common setup or constraints between actions.
+  def set_comment
+    @comment = Comment.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The comment you're looking for cannot be found"
+    respond_to do |format|
+      format.html {
+        redirect_to comments_path
+      }
     end
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def comment_params
+    params.require(:comment).permit(:message, :visible, :article_id, :user_id)
+    # Students, make sure to add the user_id and article ID parameter as symbols here ^^^^^^
+  end
 end
